6e863e616e715802fb14f2a652d2ac01c88c8fcf
Putta Khunchalee <ultimaweapon@outlook.com>
2015-03-21 20:36:16 +0700

    Initializes DebugConsole

---
+++ DebugConsole/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Putta Khunchalee">
+//     Copyright (C) 2015 Putta Khunchalee. Licensed under GNU GPLv3.
+// </copyright>
+//-----------------------------------------------------------------------
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("Debug Console")]
+[assembly: AssemblyDescription("Debug Console for YuriExtension")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Putta Khunchalee")]
+[assembly: AssemblyProduct("YuriExtension")]
+[assembly: AssemblyCopyright("Copyright © 2015 Putta Khunchalee")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: AssemblyVersion("1.0.5.0")]
+[assembly: AssemblyFileVersion("1.0.5.0")]
